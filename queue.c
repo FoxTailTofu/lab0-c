32d94b05746f6176504fa8fb5be3d308a98689b4
FoxTofu <merrwter@gmail.com>
2024-03-04 02:46:03 +0800

    A testing commit message

--- queue.c
+++ queue.c
@@ -4,6 +4,10 @@
 
 #include "queue.h"
 
+/*
+ * This is a testing comment for testing git commit
+ */
+
 /* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
  * but some of them cannot occur. You can suppress them by adding the
  * following line.
